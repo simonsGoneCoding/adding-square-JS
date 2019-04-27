88d66e16f8150977699ef9b1e563fdf4698ce078
Szymon <szymonwarszawa@gmail.com>
2019-04-27 16:08:06 +0100

    JS practice

---
+++ script.js
@@ -0,0 +1,13 @@
+// const btn = document.querySelector('button');
+// let number = 1;
+// const addElement = function () {
+//     const divItem = document.createElement('div');
+//     if (number % 5 == 0) {
+//         divItem.classList.add('circle');
+//     }
+//     document.body.appendChild(divItem);
+//     divItem.textContent = number;
+//     number++;
+// }
+
+// btn.addEventListener('click', addElement)
